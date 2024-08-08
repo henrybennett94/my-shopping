bf7a672eb64cfa17c3730b65fa83e94a35ff83c7
Henry Bennet <hwbennett94@gmail.com>
2024-08-08 11:47:32 +0000

    Add a list model

--- my_shopping/settings.py
+++ my_shopping/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-henrybennett-myshopping-1u529v4x538.ws.codeinstitute-ide.net', '.herokuapp.com',]
 
